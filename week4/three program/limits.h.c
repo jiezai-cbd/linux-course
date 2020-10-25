6ab5f8e109b3827f97d0b64116c2554976f2aecf
jiezai-cbd <wwjpcx@163.com>
2020-10-25 21:52:54 +0800

    week4 homework

---
+++ week4/three program/limits.h.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<limits.h>
+#include<float.h>
+int main()
+{
+	printf("INT min=%d,INT max=%d\n",INT_MIN,INT_MAX);
+	printf("FLOAT min=%d,FLOAT max=%d\n",FLT_MIN,FLT_MAX);
+	printf("DOUBLE min=%d,DOUBLE max=%d\n",DBL_MIN,DBL_MAX);
+	return 0;
+}
